c22f39b413eabd551ddafcc724a36c4b177969b6
Yann 'Eox' Grandgirard <iameox@outlook.fr>
2023-01-29 20:00:56 +0100

    Added channels property to IServer

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 export * from './messages';
-export * from './servers';
 export * from './sockets';
--- src/servers/IServer.ts
+++ src/servers/IServer.ts
@@ -1,7 +1,8 @@
-import { IServerRequest, IServerRoute } from 'servers';
+import { IServerChannel, IServerRequest, IServerRoute } from 'servers';
 
 export interface IServer<TRequest extends IServerRequest> {
     readonly routes: Set<IServerRoute<TRequest>>;
+    readonly channels: Map<string, IServerChannel>;
 
     close(): Promise<void>;
 }
