1eed669ab2205ff2e2e316f740b629c3208e2f54
Yann 'Eox' Grandgirard <iameox@outlook.fr>
2023-01-29 18:03:57 +0100

    Added path property to IServerRoute

--- src/servers/routes/IServerRoute.ts
+++ src/servers/routes/IServerRoute.ts
@@ -2,6 +2,8 @@ import { IServerRequest } from 'servers';
 import { ISocket } from 'sockets';
 
 export interface IServerRoute<TRequest extends IServerRequest> {
+    path: string;
+
     onUpgrade(request: TRequest): Promise<void>;
     onOpen(socket: ISocket, request: TRequest): Promise<void>;
     onClose(socket: ISocket): Promise<void>;
